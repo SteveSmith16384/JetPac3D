d945a4c10b1978a01c9e1288291b22efbf0726ce
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2021-04-15 10:27:58 +0100

    Re-enabled enemies

--- src/Globals.gd
+++ src/Globals.gd
@@ -3,7 +3,7 @@ extends Node
 const VERSION = "0.1"
 const RELEASE_MODE = false
 #const DEBUG_START_POS = true and !RELEASE_MODE # If true, uses StartPos in main for player's start position
-const NO_ENEMIES = true and !RELEASE_MODE
+const NO_ENEMIES = false and !RELEASE_MODE
 const SHOW_FPS = true and !RELEASE_MODE
 const START_LIVES = 9
 
